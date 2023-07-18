a493bce6a3c4b4087599286b9d373deabe1ce7ad
unknown <rv8828120@gmail.com>
2023-07-18 20:31:28 +0800

    Added label for HDCF

--- AddEntry.py
+++ AddEntry.py
@@ -8,6 +8,12 @@ class AddEntry(tk.Tk):
         self.title("Add Entry")
         self.geometry("700x500")
 
+        HDCF = tk.Label(self, text="Pursuant to Republic Act 11332, you are required to provide truthful information", font=("Helvetica", 12, "bold"))
+        HDCF.place(x=50, y=20)
+
+        HCDF = tk.Label(self, text="about your health condition and possible exposure.", font=("Helvetica", 12, "bold"))
+        HCDF.place(x=140, y=40)
+
 if __name__ == "__main__":
     entry = AddEntry()
     entry.mainloop()
