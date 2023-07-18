0a582a1cc9f5b044dee325cb3ca24115c3b82fc1
unknown <rv8828120@gmail.com>
2023-07-18 21:08:51 +0800

    Added search entry box to search entry ,py

--- SearchEntry.py
+++ SearchEntry.py
@@ -8,6 +8,12 @@ class SearchEntry(tk.Tk):
         self.title("Search Entry")
         self.geometry("700x500")
 
+        search = tk.Label(self, text="Search")
+        search.grid(row=2, column=0, sticky=tk.E)
+        
+        SearchEntry = tk.Entry(self)
+        SearchEntry.grid(row=2, column=1)
+
 if __name__ == "__main__":
     search = SearchEntry()
     search.mainloop()
