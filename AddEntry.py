903ad95c58c64fc9618c8bf5c2c4c9d0573580c1
unknown <rv8828120@gmail.com>
2023-07-18 20:39:38 +0800

    Name label and entry box

--- AddEntry.py
+++ AddEntry.py
@@ -14,6 +14,14 @@ class AddEntry(tk.Tk):
         HCDF = tk.Label(self, text="about your health condition and possible exposure.", font=("Helvetica", 12, "bold"))
         HCDF.place(x=140, y=40)
 
+        #Create a Label
+        name_label = tk.Label(self, text="Name:")
+        name_label.place(x=5, y=80)
+
+        #Create an entry box
+        name_entry = tk.Entry(self, width=50)  
+        name_entry.place(x=120, y=80)
+
 if __name__ == "__main__":
     entry = AddEntry()
     entry.mainloop()
