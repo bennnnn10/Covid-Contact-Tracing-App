2e2bd529e70373f8c06e59a9395754fcd32a5440
unknown <rv8828120@gmail.com>
2023-07-18 12:07:06 +0800

    Added window and label

--- Covid_Contact_Tracing_App.py
+++ Covid_Contact_Tracing_App.py
@@ -5,3 +5,12 @@
 import tkinter as tk
 
 class AddSearch(tk.Tk):
+    
+    def __init__(self):
+        super().__init__()
+
+        self.title("Contact Tracing App")
+        self.geometry("700x500")
+
+        self.header_label = tk.Label(self, text="Covid-19: Contact Tracing App", font=("Helvetica", 14, "bold"))
+        self.header_label.pack
