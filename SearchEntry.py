005c0482947490748fb25a56ed02673f356d69b0
unknown <rv8828120@gmail.com>
2023-07-19 18:35:21 +0800

    Added background image-Search Entry

--- SearchEntry.py
+++ SearchEntry.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
 class SearchEntry(tk.Tk):
 
@@ -7,6 +8,15 @@ class SearchEntry(tk.Tk):
 
         self.title("Search Entry")
         self.geometry("700x500")
+
+         # Load the background image
+        bg_image = Image.open(r"C:\Users\Ruben\OneDrive\Pictures\Covid-19\covid.jpg")
+        bg_image = bg_image.resize((900, 500))
+        self.background = ImageTk.PhotoImage(bg_image)
+
+        # Create a Label widget to display the background image
+        bg_label = tk.Label(self, image=self.background, highlightthickness=0)
+        bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
         search = tk.Label(self, text="Search")
         search.grid(row=2, column=0, sticky=tk.E)
