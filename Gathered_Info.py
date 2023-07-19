1012f8c2650897f03902ed241c0c53a973428f68
unknown <rv8828120@gmail.com>
2023-07-20 01:43:46 +0800

    Added back and close button

--- Gathered_Info.py
+++ Gathered_Info.py
@@ -18,6 +18,25 @@ class GatheredInformation(tk.Tk):
         bg_label = tk.Label(self, image=self.background, highlightthickness=0)
         bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
+        # Create a Label widget to display the background image
+        bg_label = tk.Label(self, image=self.background, highlightthickness=0)
+        bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+        go_back_to_main = tk.Button(self, text="Back", command=self.back_to_main)
+        go_back_to_main.place(x=300, y=450)
+
+        close = tk.Button(self, text="Close", command=self.close_window)
+        close.place(x=360, y=450)
+
+    def close_window(self):
+        self.destroy()
+
+    def back_to_main(self):
+        self.destroy()
+        from Covid_Contact_Tracing_App import AddSearch
+        entry = AddSearch()
+        entry.mainloop()
+
 if __name__ == "__main__":
     info = GatheredInformation()
     info.mainloop()
