ecf1120b8016cad170f0f371d65165971d719e99
unknown <rv8828120@gmail.com>
2023-07-19 20:34:33 +0800

    Added button design

--- AddEntry.py
+++ AddEntry.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from PIL import Image, ImageTk
+from tkinter import ttk
 
 class AddEntry(tk.Tk):
 
@@ -66,79 +67,82 @@ class AddEntry(tk.Tk):
         var7 = tk.IntVar()
         var8 = tk.IntVar()
 
+        style = ttk.Style()
+        style.configure("Custom.TRadiobutton", background="lightgray", foreground="black")
+
         questions = tk.Label(self, text="Do you and/or your child have the following or have had the following in the last 14 days?", font=("Helvetica", 11, "bold"))
         questions.place(x=33, y=180)
         
         yes_check_button_1_label = tk.Label(self, text="Fever or chills?", font=("Helvetica", 11, "bold"))
         yes_check_button_1_label.place(x=33, y=220)
 
-        yes_check_button_1 = tk.Radiobutton(self, variable=var1, value=1, text="Yes")
+        yes_check_button_1 = ttk.Radiobutton(self, variable=var1, value=1, text="Yes", style="Custom.TRadiobutton")
         yes_check_button_1.place(x=33, y=250)
 
-        no_check_button_1 = tk.Radiobutton(self, variable=var1, value=0, text="No")
+        no_check_button_1 = ttk.Radiobutton(self, variable=var1, value=0, text="No", style="Custom.TRadiobutton")
         no_check_button_1.place(x=100, y=250)
 
         check_button_2_label = tk.Label(self, text="Loss of smell, loss of taste?", font=("Helvetica", 11, "bold"))
         check_button_2_label.place(x=33, y=290)
 
-        yes_check_button_2 = tk.Radiobutton(self, variable=var2, value=1, text="Yes")
+        yes_check_button_2 = ttk.Radiobutton(self, variable=var2, value=1, text="Yes", style="Custom.TRadiobutton")
         yes_check_button_2.place(x=33, y=320)
 
-        no_check_button_2 = tk.Radiobutton(self, variable=var2, value=0, text="No")
+        no_check_button_2 = ttk.Radiobutton(self, variable=var2, value=0, text="No", style="Custom.TRadiobutton")
         no_check_button_2.place(x=100, y=320)
 
         yes_check_button_3_label = tk.Label(self, text="Cough, colds, sore throat?", font=("Helvetica", 11, "bold"))
         yes_check_button_3_label.place(x=180, y=220)
 
-        yes_check_button_3 = tk.Radiobutton(self, variable=var3, value=1, text="Yes")
+        yes_check_button_3 = ttk.Radiobutton(self, variable=var3, value=1, text="Yes", style="Custom.TRadiobutton")
         yes_check_button_3.place(x=180, y=250)
 
-        no_check_button_3 = tk.Radiobutton(self, variable=var3, value=0, text="No")
+        no_check_button_3 = ttk.Radiobutton(self, variable=var3, value=0, text="No", style="Custom.TRadiobutton")
         no_check_button_3.place(x=247, y=250)
 
         yes_check_button_4_label = tk.Label(self, text="Diarrhea?", font=("Helvetica", 11, "bold"))
         yes_check_button_4_label.place(x=405, y=220)
 
-        yes_check_button_4 = tk.Radiobutton(self, variable=var4, value=1, text="Yes")
+        yes_check_button_4 = ttk.Radiobutton(self, variable=var4, value=1, text="Yes", style="Custom.TRadiobutton")
         yes_check_button_4.place(x=405, y=250)
 
-        no_check_button_4 = tk.Radiobutton(self, variable=var4, value=0, text="No")
+        no_check_button_4 = ttk.Radiobutton(self, variable=var4, value=0, text="No", style="Custom.TRadiobutton")
         no_check_button_4.place(x=472, y=250)
 
         yes_check_button_5_label = tk.Label(self, text="Muscle pain?", font=("Helvetica", 11, "bold"))
         yes_check_button_5_label.place(x=550, y=220)
 
-        yes_check_button_5 = tk.Radiobutton(self, variable=var5, value=1, text="Yes")
+        yes_check_button_5 = ttk.Radiobutton(self, variable=var5, value=1, text="Yes", style="Custom.TRadiobutton")
         yes_check_button_5.place(x=550, y=250)
 
-        no_check_button_5 = tk.Radiobutton(self, variable=var5, value=0, text="No")
+        no_check_button_5 = ttk.Radiobutton(self, variable=var5, value=0, text="No", style="Custom.TRadiobutton")
         no_check_button_5.place(x=617, y=250)
 
         check_button_6_label = tk.Label(self, text="Shortness of breath?", font=("Helvetica", 11, "bold"))
         check_button_6_label.place(x=270, y=290)
 
-        yes_check_button_6 = tk.Radiobutton(self, variable=var6, value=1, text="Yes")
+        yes_check_button_6 = ttk.Radiobutton(self, variable=var6, value=1, text="Yes", style="Custom.TRadiobutton")
         yes_check_button_6.place(x=270, y=320)
 
-        no_check_button_6 = tk.Radiobutton(self, variable=var6, value=0, text="No")
+        no_check_button_6 = ttk.Radiobutton(self, variable=var6, value=0, text="No", style="Custom.TRadiobutton")
         no_check_button_6.place(x=337, y=320)
 
-        check_button_7_label = tk.Label(self, text="Excessive Fatigue?", font=("Helvetica", 11, "bold"))
+        check_button_7_label = ttk.Label(self, text="Excessive Fatigue?", font=("Helvetica", 11, "bold"))
         check_button_7_label.place(x=480, y=290)
 
-        yes_check_button_7 = tk.Radiobutton(self, variable=var7, value=1, text="Yes")
+        yes_check_button_7 = ttk.Radiobutton(self, variable=var7, value=1, text="Yes", style="Custom.TRadiobutton")
         yes_check_button_7.place(x=480, y=320)
 
-        no_check_button_7= tk.Radiobutton(self, variable=var7, value=0, text="No")
+        no_check_button_7= ttk.Radiobutton(self, variable=var7, value=0, text="No", style="Custom.TRadiobutton")
         no_check_button_7.place(x=547, y=320)
 
         label_8 = tk.Label(self, text="Did you consult a medical doctor for the above mentioned signs and symptoms?", font=("Helvetica", 11, "bold"))
         label_8.place(x=70, y=370)
 
-        yes_label_8_button = tk.Radiobutton(self, variable=var8, value=1, text="Yes")
+        yes_label_8_button = ttk.Radiobutton(self, variable=var8, value=1, text="Yes", style="Custom.TRadiobutton")
         yes_label_8_button.place(x=300, y=400)
 
-        no_label_8_button = tk.Radiobutton(self, variable=var8, value=0, text="No")
+        no_label_8_button = ttk.Radiobutton(self, variable=var8, value=0, text="No", style="Custom.TRadiobutton")
         no_label_8_button.place(x=360, y=400)
          
         #Submit Button
