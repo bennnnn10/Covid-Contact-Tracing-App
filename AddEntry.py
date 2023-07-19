20329c66eb1f561d989ed2d010d2da26875a44bb
unknown <rv8828120@gmail.com>
2023-07-19 17:47:36 +0800

    Added label radiobutton for fever and chills question

--- AddEntry.py
+++ AddEntry.py
@@ -56,11 +56,20 @@ class AddEntry(tk.Tk):
         address_entry = tk.Entry(self, width=93) 
         address_entry.place(x=101, y=140)
 
+        #Check button
+        var1 = tk.IntVar()
+
         questions = tk.Label(self, text="Do you and/or your child have the following or have had the following in the last 14 days?", font=("Helvetica", 11, "bold"))
         questions.place(x=33, y=180)
         
         yes_check_button_1_label = tk.Label(self, text="Fever or chills?", font=("Helvetica", 11, "bold"))
         yes_check_button_1_label.place(x=33, y=220)
+
+        yes_check_button_1 = tk.Radiobutton(self, variable=var1, value=1, text="Yes")
+        yes_check_button_1.place(x=33, y=250)
+
+        no_check_button_1 = tk.Radiobutton(self, variable=var1, value=0, text="No")
+        no_check_button_1.place(x=100, y=250)
 
 if __name__ == "__main__":
     entry = AddEntry()
