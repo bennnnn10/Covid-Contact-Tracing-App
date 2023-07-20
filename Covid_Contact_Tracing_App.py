8c3f0c953cb60d78dc0a3832ae891b148d6a5c84
unknown <rv8828120@gmail.com>
2023-07-20 22:21:50 +0800

    Close button

--- Covid_Contact_Tracing_App.py
+++ Covid_Contact_Tracing_App.py
@@ -30,6 +30,9 @@ class AddSearch(tk.Tk):
 
         SearchEntryButton = tk.Button(self, text="Search Entry", command=self.open_search_entry)
         SearchEntryButton.place(x=313, y=310)
+        
+        close = tk.Button(self, text="Close", command=self.close_window)
+        close.place(x=335, y=450)
 
     def open_add_entry(self):
         self.destroy()
@@ -40,6 +43,9 @@ class AddSearch(tk.Tk):
         self.destroy()
         search = SearchEntry()
         search.mainloop()
+
+    def close_window(self):
+        self.destroy()
 
 if __name__ == "__main__":
     app = AddSearch()
